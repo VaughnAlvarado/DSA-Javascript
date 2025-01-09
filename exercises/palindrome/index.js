70807375f13b990f9fff80ed73d27343b7c619ba
Vaughn Alvarado <vaughn@parlevelsystems.com>
2025-01-08 22:22:35 -0600

    first commit

---
+++ exercises/palindrome/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+
+// ============================================= FIRST VERSION
+
+// function palindrome(str) {
+//     reversed = str.split('').reduce((rev, char) => char + rev , '')
+//     return str == reversed
+// }
+
+// ============================================= SECOND VERSION
+// This uses the every function, which digests the array and you pass in the value you iterate 1st param with index as 2nd param
+// if every result is true, it passes true otherwise its false, here we check that every beginning char matches the last
+// ex 0 index matches last index, 1 index matches 2nd to last index, etc
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - (i + 1)]
+    })
+}
+
+
+
+module.exports = palindrome;
---
+++ exercises/reversestring/index.js
@@ -0,0 +1,50 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// ========================= FIRST VERSION
+// function reverse(str) {
+//     let newStr = '';
+//     const len = str.length;
+
+//     for (let i = len - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr
+// }
+
+
+// ========================= THIRD VERSION
+
+function reverse(str) {
+    let reversed = '';
+
+    for (let char of str) {
+        reversed = char + reversed;
+        debugger;
+
+    }
+    return reversed
+}
+
+// ========================= SECOND VERSION
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// ========================= FOURTH VERSION
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((reversed, char) => {
+//         return char + reversed;
+//     }, '');
+    // can also format as 
+    // return str.split('').reduce((reversed, char) => char + reversed, '');
+// }
+reverse('fgdfgdfgdf');
+
+module.exports = reverse;
