bedde1246f896e84b7924d4920f2d7ac648ab29b
Vaughn Alvarado <vaughn@parlevelsystems.com>
2025-01-23 23:07:50 -0600

    adding array chunking function

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,57 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+// FIRST VERSION
+// function chunk(array, size) {
+//     let result = [];
+//     let arrChunk = [];
+
+//     while (array.length) {
+//         arrChunk.push(array.shift())
+
+//         if (arrChunk.length == size) {
+//             result.push(arrChunk);
+//             arrChunk = [];
+//         }
+//     }
+
+//     if (arrChunk.length) {
+//         result.push(arrChunk);
+//     }
+
+//     return result;
+// }
+
+
+// SECOND VERSION
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let num of array) {
+//         const last = chunked[chunked.length - 1];
+
+//         if (!last || last.length === size) {
+//             chunked.push([num]);
+//         } else {
+//             last.push(num);
+//         }
+//     }
+
+//     return chunked;
+// }
+
+// THIRD VERSION
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    
+    return chunked;
+}
 
 module.exports = chunk;
