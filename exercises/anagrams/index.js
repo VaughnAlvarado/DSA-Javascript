82f39aa2e9271b4cd9ebbaf37272ef41f806fcc7
Vaughn Alvarado <vaughn@parlevelsystems.com>
2025-01-23 23:57:47 -0600

    adding code to detect if two strings are a anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// First Version
+function anagrams(stringA, stringB) {
+    const aStrMap = buildStrMap(stringA);
+    const bStrMap = buildStrMap(stringB);
+
+    if (Object.keys(aStrMap).length !== Object.keys(bStrMap).length) {
+        return false;
+    }
+
+    for (let key in bStrMap) {
+        if (bStrMap[key] !== aStrMap[key]) {
+            return false
+        }
+    }   
+    return true;
+}
+
+function buildStrMap(str) {
+
+    const charMap = {}
+    const strStripped = str.replace(/[^\w]/g, "").toLowerCase();
+
+    for (let char of strStripped) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+
+// Second Version 
+// function anagrams(stringA, stringB) {
+//     return cleanString(stringA) === cleanString(stringB);
+// }
+
+// function cleanString(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
+// }
+
 
 module.exports = anagrams;
