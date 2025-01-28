72c0b9b7df0aaf21d3a5de5739871aa97e36cf98
Vaughn Alvarado <alvaradovaughn@yahoo.com>
2025-01-27 22:42:01 -0600

    adding back capitalize functions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,5 +8,36 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 
+// First Version 
+function capitalize(str) {
+    return str.split(' ').map((strSection) => {
+        return strSection[0].toUpperCase() + strSection.slice(1);
+    }).join(' ');
+}
+
+// Second Version
+// function capitalize(str){
+//     const words = [];
+
+//     for (let word of str.split('')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
+
+// THIRD Version
+// function capitalize(str){
+//     let result = str[0].toUpperCase();
+
+//     for (let i = 1; i < str.length; i++) {
+//         if (str[i - 1] === ' ') {
+//             result += str[i].toUpperCase();
+//         } else {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
 
 module.exports = capitalize;
